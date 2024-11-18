c5d2ef0c7426661e06401512417e4611dc76762e
jimao <shihao.gu@perfma.com>
2024-11-18 14:58:26 +0800

    nanoid

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import dayjs from 'dayjs';
+import { nanoid } from 'nanoid';
 
 export default
 class MyClass {
   public hello() {
-    console.log('你好世界', dayjs().format('YYYY-MM-DD HH:mm:ss'));
+    console.log('你好世界', dayjs().format('YYYY-MM-DD HH:mm:ss'), nanoid());
   }
 }
 
